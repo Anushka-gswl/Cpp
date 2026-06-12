8d9bcdbc088898058ede7af500cfa2ca5976734c
Anushka-gswl <anu1108.gswl@gmail.com>
2026-06-12 22:45:20 +0530

    eighth commit

---
+++ DSA/Functions/MostWater.cpp
@@ -0,0 +1,11 @@
+int maxArea(vector<int>& height) {
+    int maxWater = 0;
+    for( int i = 0; i < height.size(); i++){
+        for(int j = i+1; j < height.size(); j++){
+            int w = j - i;
+            int h = min( height [i], height [j]);
+            maxWater = max (maxWater, w * h);
+        }
+    }
+    return maxWater;
+}
