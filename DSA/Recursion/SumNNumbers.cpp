2289ce53ada676659b35a5c5946537367387d114
Anushka-gswl <anu1108.gswl@gmail.com>
2026-06-17 22:11:44 +0530

    17th commit

---
+++ DSA/Recursion/SumNNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int sumN(int n){
+    if(n==1){
+        return 1;
+    }
+    return n + sumN(n - 1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout << sumN(n);
+    return 0;
+}
