80833b197566a9c716e66c60350f91d874b0d5d2
Anushka-gswl <anu1108.gswl@gmail.com>
2026-07-01 00:43:53 +0530

    36th commit

--- DSA/Graphs/BellManFord.cpp
+++ DSA/Graphs/BellManFord.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <climits>
 using namespace std;
 
 //BellMan Ford Algo can also be used for negatively weighted graphs
 //DP Algo
+//Doesn't work for negative weight cycles
+
+//TC => O(V.E)
+
+class Edge{
+public:
+    int v;
+    int wt;
+ 
+    Edge(int v, int wt){
+        this->v = v;
+        this->wt = wt;
+    }
+};
+
+void bellmanFord(int src, vector<vector<Edge>> g){
+    int V = g.size();
+    vector<int> dist(V, INT_MAX);
+
+    dist[src] = 0;
+    
+    for(int i = 0; i < V - 1; i++){
+        for(int u = 0; u < V; u++){
+            for(Edge e : g[u]){
+                if(dist[e.v] > dist[u] + e.wt){
+                    dist[e.v] = dist[u] + e.wt;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < V; i++){
+        cout << dist[i] << " ";
+    }
+}
+
+int main(){
+    int V = 5;
+    vector<vector<Edge>> g(V);
+
+    g[0].push_back(Edge(1,2));
+    g[0].push_back(Edge(2,4));
+
+    g[1].push_back(Edge(4,-1));
+    g[1].push_back(Edge(2,-4));
+
+    g[2].push_back(Edge(3,2));
+
+    g[3].push_back(Edge(4,4));
+
+    bellmanFord(0, g);
+
+    return 0;
+}
 
---
+++ DSA/Graphs/PrimsAlgo.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+//Prim's Algo is only used for undirected, weighted and connected graphs
+//Greedy Algo
+
+//
+
+class Edge{
+public:
+    int v;
+    int wt;
+ 
+    Edge(int v, int wt){
+        this->v = v;
+        this->wt = wt;
+    }
+};
+
+void prims(int src, vector<vector<Edge>> g){
+    int V = g.size();
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq; //pair<edge wt, neighbour>
+    vector<bool> inMST(V, false);
+    int minCost = 0;
+
+    pq.push({0,src});
+
+    while(!pq.empty()){
+        int wt = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if(!inMST[u]){
+            inMST[u] = true;
+            minCost += wt;
+            for(Edge e : g[u]){
+                if(!inMST[e.v]){
+                    pq.push({e.wt, e.v});
+                }
+            }
+        }
+    }
+
+    cout << minCost;
+}
+
+int main(){
+    int V = 6;
+    vector<vector<Edge>> g(V);
+
+    g[0].push_back(Edge(1,10));
+    g[0].push_back(Edge(3,30));
+    g[0].push_back(Edge(2,15));
+
+    g[1].push_back(Edge(3,40));
+
+    g[3].push_back(Edge(2,50));
+
+    g[1].push_back(Edge(0,10));
+    g[3].push_back(Edge(0,30));
+    g[2].push_back(Edge(0,15));
+
+    g[3].push_back(Edge(1,40));
+
+    g[2].push_back(Edge(3,50));
+
+    prims(0, g);
+
+    return 0;
+}
