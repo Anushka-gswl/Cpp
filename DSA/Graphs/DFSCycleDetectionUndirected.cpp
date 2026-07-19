6ba37a49f26e4ea94f03af29d1ac68e7723fd587
Anushka-gswl <anu1108.gswl@gmail.com>
2026-07-19 22:34:26 +0530

    46th commit

--- DSA/Graphs/DFSCycleDetectionUndirected.cpp
+++ DSA/Graphs/DFSCycleDetectionUndirected.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <list>
 using namespace std;
-//Cycle Detection in Undirected Graph
+//Cycle Detection in Undirected Graph using DFS
 class Graph{
     int V;
     list<int> *l;
